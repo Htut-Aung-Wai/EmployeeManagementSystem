a5057c7ae121689eaeaac501f44e20b4eae38aa4
Htut Aung Wai <htutaungwai333@gmail.com>
2025-01-16 15:08:54 +0630

    final_raw

--- src/EmployeeService.java
+++ src/EmployeeService.java
@@ -1,8 +1,102 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class EmployeeService {
 
-    //public String employeeList()
-    {
+
+    static ArrayList<Employee> EmployeeID = new ArrayList<>();
+
+    public void addEmployee(String id, String name, Double salary) {
+        Employee add = new Employee(id, name, salary);
+        EmployeeID.add(add);
+
+    }
+
+    public void EmployeeAddOption() {
+
+        String option = "N";
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.println("Enter Employee ID");
+        String id = in.nextLine();
+
+        System.out.println("Enter Employee Name");
+        String name = in.nextLine();
+
+        System.out.println("Enter Employee Salary");
+        Double salary = in.nextDouble();
+        addEmployee(id, name, salary);
+        in.nextLine();
+        System.out.println("Do you want to add more (Y or N)");
+        option = in.nextLine();
+        if (option.equals("Y")) {
+            EmployeeAddOption();
+        } else {
+            return;
+        }
+
+
+        in.close();
+    }
+
+    public void employeeSearch(String Employee) {
+
+        for (Employee list : EmployeeID) {
+
+            if (Employee.equals(list.getID())) {
+                System.out.println("Employee Exists ! We found it !!");
+                System.out.println("ID : " + list.getID() + " name : " + list.getName() + " Salary : " + list.getSalary());
+            } else {
+                System.out.println("Employee not Found !!");
+            }
+
+
+        }
+
+    }
+
+    public void AllemployeeDisplay() {
+        for (Employee list : EmployeeID) {
+            System.out.println("All Employee Display");
+            System.out.println("ID : " + list.getID() + " name : " + list.getName() + " Salary : " + list.getSalary());
+
+
+        }
+    }
+
+    public void employeeSalaryUpdate(String employeeid, double salary) {
+        for (Employee list : EmployeeID) {
+
+            if (employeeid.equals(list.getID())) {
+                list.UpdateSalary(salary);
+                System.out.println(list.getName() + " 's Salary Updated to " + salary);
+            } else {
+
+                System.out.println("Employee not found !!");
+
+            }
+
+
+        }
+
+    }
+
+    public void calculateSalaryPercent(String id, int percent) {
+
+        for (Employee list : EmployeeID) {
+
+            if (id.equals(list.getID())) {
+                double bonus = list.CalculateBonus(percent);
+                System.out.println(list.getName() + " 's Bonus is " + bonus);
+            }
+            else{
+                System.out.println("Employee not found or Input is wrong");
+            }
+
+        }
 
 
     }
+
 }
--- src/main.java
+++ src/main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class main {
 
-    static ArrayList<Employee> EmployeeID = new ArrayList<>();
+    /*static ArrayList<Employee> EmployeeID = new ArrayList<>();
 
     public static void addEmployee(String id,String name,Double salary)
     {
@@ -16,10 +16,11 @@ public class main {
     public static void EmployeeAddOption()
     {
 
-        String option="Y";
+        String option="N";
         Scanner in =new Scanner(System.in);
 
-        while (!option.equals("N")) {
+
+
 
 
             System.out.println("Enter Employee ID");
@@ -34,18 +35,25 @@ public class main {
             in.nextLine();
             System.out.println("Do you want to add more (Y or N)");
             option=in.nextLine();
-        }
+            if (option.equals("Y"))
+            {
+                EmployeeAddOption();
+            }
+            else {
+                return;
+            }
+
 
         in.close();
-    }
+    } */
 
-    public void employeeSearch(String Employee)
+    /*public static void employeeSearch(String Employee)
     {
 
         for(Employee list: EmployeeID) {
 
             if (Employee.equals(list.getID())) {
-                System.out.println("Employee Exists");
+                System.out.println("Employee Exists ! We found it !!");
                 System.out.println("ID : "+list.getID()+" name : "+list.getName()+" Salary : "+list.getSalary());
             }
 
@@ -53,30 +61,73 @@ public class main {
 
     }
 
+    public static void AllemployeeDisplay()
+    {
+        for (Employee list : EmployeeID)
+        {
+            System.out.println("All Employee Display");
+            System.out.println("ID : " + list.getID() + " name : " + list.getName() + " Salary : " + list.getSalary());
+
+
+        }
+    }*/
+
+
 
     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
-        System.out.print("Enter Employee ID");
-        String employeeID=in.nextLine();
+        String option="Y";
 
-        System.out.println("Enter your option");
-        int inputoption= in.nextInt();
-        switch(inputoption){
+        while (!option.equals("N")) {
+            EmployeeService ES=new EmployeeService();
+            System.out.print("Enter Employee ID");
+            String employeeID = in.nextLine();
 
-            case 1: {EmployeeAddOption();}
+            System.out.println("Enter your option");
+            int inputoption = in.nextInt();
+            switch (inputoption) {
 
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + inputoption);
-        }
+                case 1: {
+                    ES.EmployeeAddOption();
+                    break;
+
+                }
+
+                case 2:{
+
+                    ES.employeeSearch(employeeID);
+                    break;
+                }
+
+                case 3:{
+
+                    ES.AllemployeeDisplay();
+                    break;
+                }
+                case 4:{
+                    System.out.println("Enter Update Salary");
+                    double salary=in.nextDouble();
+                    ES.employeeSalaryUpdate(employeeID,salary);
+                    break;
+                }
+                case 5:{
+                    System.out.println("Enter bonus percent");
+                    int bonuspercent=in.nextInt();
+                    ES.calculateSalaryPercent(employeeID,bonuspercent);
+                    break;
+                }
+
+
+                default:
+                    throw new IllegalStateException("Unexpected value: " + inputoption);
+            }
 
 
 
 
-        for(Employee list:EmployeeID) {
-              System.out.println("ID : "+list.getID()+" name : "+list.getName()+" Salary : "+list.getSalary());
-
-
+            System.out.println("Do you want to continue (Y or N)");
+            in.nextLine();
+            option=in.nextLine();
         }
 
 
