363c053ee4186bf286bf19085367ac0a6ad8356b
Htut-Aung-Wai <htutaungwai333@gmail.com>
2025-01-17 16:41:22 +0630

    try_catch_added_to_option_run.java

--- src/run.java
+++ src/run.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 
 public class run {
@@ -11,64 +12,78 @@ public class run {
         Scanner in =new Scanner(System.in);
         String option="Y";
 
-        while (!option.equals("N")) {
-            EmployeeService employeeServiceVar=new EmployeeService();
+        try {
 
-            System.out.println("Enter your option\n1:Add\n2:Search\n3:Display\n4:Salary Update\n5:Bonus Calculate");
-            int inputOption = in.nextInt();
-            in.nextLine();
-            switch (inputOption) {
 
-                case 1: {
-                    employeeServiceVar.EmployeeAddOption();
-                    break;
+            while (!option.equals("N")) {
+                EmployeeService employeeServiceVar = new EmployeeService();
 
+                System.out.println("Enter your option\n1:Add\n2:Search\n3:Display\n4:Salary Update\n5:Bonus Calculate\n***Only Enter between 1 to 5***");
+                int inputOption = in.nextInt();
+                in.nextLine();
+                switch (inputOption) {
+
+                    case 1: {
+                        employeeServiceVar.EmployeeAddOption();
+                        break;
+
+                    }
+
+                    case 2: {
+                        System.out.println("Enter Employee ID to search : ");
+                        String employeeIdSearch = in.nextLine();
+                        employeeServiceVar.EmployeeSearch(employeeIdSearch);
+                        break;
+                    }
+
+                    case 3: {
+
+                        employeeServiceVar.AllEmployeeDisplay();
+                        break;
+                    }
+                    case 4: {
+                        System.out.print("Enter Employee ID to update salary : ");
+                        String employeeIdUpdateSalary = in.nextLine();
+                        System.out.println("Enter Update Salary");
+                        double salary = in.nextDouble();
+                        in.nextLine();
+                        employeeServiceVar.EmployeeSalaryUpdate(employeeIdUpdateSalary, salary);
+                        break;
+                    }
+                    case 5: {
+                        System.out.print("Enter Employee ID : ");
+                        String employeeIdSalaryPercent = in.nextLine();
+                        System.out.println("Enter bonus percent");
+                        int bonusPercent = in.nextInt();
+                        in.nextLine();
+                        employeeServiceVar.CalculateSalaryPercent(employeeIdSalaryPercent, bonusPercent);
+                        break;
+                    }
+
+
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + inputOption);
                 }
 
-                case 2:{
-                    System.out.println("Enter Employee ID to search : ");
-                    String employeeIdSearch=in.nextLine();
-                    employeeServiceVar.EmployeeSearch(employeeIdSearch);
-                    break;
-                }
 
-                case 3:{
+                System.out.println("Do you want to continue (Y or N)");
 
-                    employeeServiceVar.AllEmployeeDisplay();
-                    break;
-                }
-                case 4:{
-                    System.out.print("Enter Employee ID to update salary : ");
-                    String employeeIdUpdateSalary = in.nextLine();
-                    System.out.println("Enter Update Salary");
-                    double salary=in.nextDouble();
-                    in.nextLine();
-                    employeeServiceVar.EmployeeSalaryUpdate(employeeIdUpdateSalary,salary);
-                    break;
-                }
-                case 5:{
-                    System.out.print("Enter Employee ID : ");
-                    String employeeIdSalaryPercent = in.nextLine();
-                    System.out.println("Enter bonus percent");
-                    int bonusPercent=in.nextInt();
-                    in.nextLine();
-                    employeeServiceVar.CalculateSalaryPercent(employeeIdSalaryPercent,bonusPercent);
-                    break;
-                }
+                option = in.nextLine();
 
-
-                default:
-                    throw new IllegalStateException("Unexpected value: " + inputOption);
             }
 
-
-
-
-            System.out.println("Do you want to continue (Y or N)");
-
-            option=in.nextLine();
-
         }
+        catch (InputMismatchException e)
+        {
+            System.out.println("Input wrong : System wouldn't accept this input.Pls restart the system ;");
+        }
+
+        catch (Exception e) {
+
+            System.out.println("Error occurred: " + e.getMessage());
+        }
+
+
 
 
 
